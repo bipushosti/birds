325672231e30c230b3167623ff9652c30404dc37
Bipush Osti <Bipush.Osti@gmail.com>
2014-12-08 19:05:29 -0500

    Data files are working.Getting the correct angle and magnitude values

--- First_run.cu
+++ First_run.cu
@@ -1,8 +1,9 @@
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<cuda.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
 
 #define PI 3.14159
 
@@ -24,35 +25,39 @@
 //Assuming Max head wind speed = 30km/hr
 
 
-//----------------------------------------------------------------------------------------------
-struct Data
-{
-	int *temp_row;
-	int *temp_col;	
-};
-
+//--------------------------------------------------------------------------------------------------------------------------
 
 __global__ void get_resultant(float * u, float* v,float* resultantMatrix,float* resultantAngle);
-struct Data invoke_Computekernel(float* resultantMatrix,float* resultantAngle);
 
-//----------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------
 
 //Kernel to get angle and magnitude from u and v matrices
 __global__ void get_resultant(float * u, float* v,float* resultantMatrix,float* resultantAngle)
 {
-	float magnitude,angle;
+	float angle;
 
 	int x = (blockIdx.x * blockDim.x) + threadIdx.x;
 	int y = (blockIdx.y * blockDim.y) + threadIdx.y;
+	int index = x + y * XSIZE; 
 
-	magnitude = hypotf(u[y * XSIZE + x],v[y * XSIZE + x]);
-	angle = asin(u[y * XSIZE + x]/v[y * XSIZE + x]) * 180/PI;
-	resultantMatrix[y * XSIZE + x] = magnitude;
-	resultantAngle[y * XSIZE  + x] = angle;
+	if(x < 55) {
+		resultantMatrix[index] = hypotf(u[index],v[index]);
+		angle = asin(u[index]/resultantMatrix[index]) * 180/PI;
+		if(angle < 0){
+			angle = (360 - angle);
+		}
+		if(angle > 360) {
+			angle = angle - 360;
+		}
+		resultantAngle[index] = angle;
+		printf("%f,%f\n",resultantMatrix[index],angle);
+	}	
+
 }
 
+/*
 //Computation Kernel
-__global__ void bird_thread(float* resultantMatrix,float* resultantAngle)
+__global__ void bird_thread(float* resultantMatrix,float* resultantAngle,int* coords_row,int* coords_col)
 {
 	int x = (blockIdx.x * blockDim.x) + threadIdx.x;
 	int y = (blockIdx.y * blockDim.y) + threadIdx.y;	
@@ -95,24 +100,34 @@ __global__ void bird_thread(float* resultantMatrix,float* resultantAngle)
 			pos_col -= 1;
 		}
 		
+		coords_row[TIMESTEPS] = pos_row;
+		coords_col[TIMESTEPS] = pos_col;
+		printf("%d,%d\n",pos_row,pos_col);		
+	
 	}
 
 	
 	
 
 }
+*/
 
 
-
-//----------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------
 int main()
 {
-	
+
+	size_t limit;
+	cudaDeviceSetLimit(cudaLimitPrintfFifoSize, 500 * 1024 * 1024);
+	cudaDeviceSetLimit(cudaLimitMallocHeapSize, 128*1024*1024);
+	cudaDeviceGetLimit(&limit,cudaLimitPrintfFifoSize);
+
 	float udata[YSIZE * XSIZE];
 	float vdata[YSIZE * XSIZE];
 
 	FILE *udataTxt;
 	FILE *vdataTxt;
+
 	udataTxt = fopen("udata.txt","r");
 	if(udataTxt == NULL) {
 		perror("Cannot open udataTxt file\n");
@@ -139,39 +154,6 @@ int main()
 	i=0;
 	j=0;
 	
-	while(fgets(line,LINESIZE,udataTxt)!=NULL){
-		startPtr = line;
-		for(i=0;i<XSIZE;i++){
-
-			Value = 0;
-			memset(tempVal,'\0',sizeof(tempVal));
-
-			if(i != (XSIZE - 1)) {
-
-				endPtr = strchr(startPtr,' ');
-				strncpy(tempVal,startPtr,endPtr-startPtr);
-				Value = atof(tempVal);
-				udata[j * XSIZE + i] = Value;
-				endPtr = endPtr + 1;
-				startPtr = endPtr;
-			}
-			else if(i == (XSIZE - 1)){
-
-				strcpy(tempVal,startPtr);
-				Value = atof(tempVal);
-				udata[j * XSIZE + i] = Value;
-			}
-		}
-		j++;
-	}	
-
-	
-	memset(line,'\0',sizeof(line));
-	memset(tempVal,'\0',sizeof(tempVal));
-
-	i=0;
-	j=0;
-
 
 	while(fgets(line,LINESIZE,vdataTxt)!=NULL){
 		startPtr = line;
@@ -195,31 +177,81 @@ int main()
 				Value = atof(tempVal);
 				vdata[j * XSIZE + i] = Value;
 			}
+			
+			
 		}
 		j++;
 	}	
 
-	
+	memset(line,'\0',sizeof(line));
+	memset(tempVal,'\0',sizeof(tempVal));
+
+	i=0;
+	j=0;
+
+	while(fgets(line,LINESIZE,udataTxt)!=NULL){
+		startPtr = line;
+		for(i=0;i<XSIZE;i++){
+
+			Value = 0;
+			memset(tempVal,'\0',sizeof(tempVal));
+
+			if(i != (XSIZE - 1)) {
+
+				endPtr = strchr(startPtr,' ');
+				strncpy(tempVal,startPtr,endPtr-startPtr);
+				Value = atof(tempVal);
+				udata[j * XSIZE + i] = Value;
+				endPtr = endPtr + 1;
+				startPtr = endPtr;
+			}
+			else if(i == (XSIZE - 1)){
+
+				strcpy(tempVal,startPtr);
+				Value = atof(tempVal);
+				udata[j * XSIZE + i] = Value;
+			}
+		//printf("%f\n",udata[j * XSIZE + i]);
+		}
+		j++;
+	}	
+
+
 	float resultantMatrix[XSIZE * YSIZE];
 	float resultantAngle[XSIZE * YSIZE];
 	
+	resultantMatrix[YSIZE * XSIZE -1] = 834.0;	
 
-	float* udataPtr,*vdataPtr,*resultantMatrixPtr,*resultantAnglePtr;
+	float *udataPtr,*vdataPtr,*resultantMatrixPtr,*resultantAnglePtr;
 
 	cudaMalloc((void**)&udataPtr,XSIZE * YSIZE * sizeof(float));
+	cudaMemcpy(udataPtr,udata,XSIZE * YSIZE * sizeof(float),cudaMemcpyHostToDevice);
+
 	cudaMalloc((void**)&vdataPtr,XSIZE * YSIZE * sizeof(float));
+	cudaMemcpy(vdataPtr,vdata,XSIZE * YSIZE * sizeof(float),cudaMemcpyHostToDevice);
+
 	cudaMalloc((void**)&resultantMatrixPtr,XSIZE * YSIZE * sizeof(float));
 	cudaMalloc((void**)&resultantAnglePtr,XSIZE * YSIZE * sizeof(float));
 	
 	
 
-	cudaMemcpy(udataPtr,udata,XSIZE * YSIZE * sizeof(float),cudaMemcpyHostToDevice);
-	cudaMemcpy(vdataPtr,udata,XSIZE * YSIZE * sizeof(float),cudaMemcpyHostToDevice);
+	
+	cudaMemcpy(vdataPtr,vdata,XSIZE * YSIZE * sizeof(float),cudaMemcpyHostToDevice);
 
-	dim3 gridSize(1,YSIZE,0);
-	dim3 blockSize(XSIZE,1,1);
-
+	dim3 gridSize(1,95,1);
+	dim3 blockSize(64,1,1);
+	printf("Hello2\n");
 	get_resultant<<<gridSize,blockSize>>>(udataPtr,vdataPtr,resultantMatrixPtr,resultantAnglePtr);
+	printf("Hello3\n");
+	cudaDeviceSynchronize();
+	cudaError_t error = cudaGetLastError();
+	if(error != cudaSuccess)
+  	{
+		printf("CUDA Error: %s\n", cudaGetErrorString(error));
+
+    	// we can't recover from the error -- exit the program
+    	return 0;
+  	}
 
 	cudaMemcpy(resultantMatrix,resultantMatrixPtr,YSIZE * XSIZE * sizeof(float),cudaMemcpyDeviceToHost);
 	cudaMemcpy(resultantAngle,resultantAnglePtr,YSIZE * XSIZE * sizeof(float),cudaMemcpyDeviceToHost);
@@ -229,49 +261,38 @@ int main()
 	cudaFree(resultantMatrixPtr);
 	cudaFree(resultantAnglePtr);
 	
-	//struct Data data_final;
 
-	//data_final = invoke_Computekernel(resultantMatrix,resultantAngle);
-	//return 0;
-//}
+	//for(i=0;i< XSIZE * YSIZE;i++) {
+	//	printf("%f\n",resultantAngle[i]);
+	//}
+//--------------------------------------------------------------------------------------------------------------------------
+	/*int coords_row[TIMESTEPS];
+	int coords_col[TIMESTEPS];
+	int *coords_rowPtr,*coords_colPtr;
 
-//Invoking computation kernel
-//struct Data invoke_Computekernel(float* resultantMatrix,float* resultantAngle)
-//{
-
-	int pos_row[TIMESTEPS];
-	int pos_col[TIMESTEPS];
-//	float *pos_rowPtr,*pos_colPtr,*resultantMatrixPtr,*resultantAnglePtr;
-	float *pos_rowPtr,*pos_colPtr;
-
-	cudaMalloc((void**)&pos_rowPtr,TIMESTEPS * sizeof(int));
-	cudaMalloc((void**)&pos_colPtr,TIMESTEPS * sizeof(int));
+	cudaMalloc((void**)&coords_rowPtr,TIMESTEPS * sizeof(int));
+	cudaMalloc((void**)&coords_colPtr,TIMESTEPS * sizeof(int));
 	cudaMalloc((void**)&resultantMatrixPtr,XSIZE * YSIZE * sizeof(float));
 	cudaMalloc((void**)&resultantAnglePtr,XSIZE * YSIZE * sizeof(float));
 
 	cudaMemcpy(resultantMatrixPtr,resultantMatrix,XSIZE * YSIZE * sizeof(float),cudaMemcpyHostToDevice);
 	cudaMemcpy(resultantAnglePtr,resultantAngle,XSIZE * YSIZE * sizeof(float),cudaMemcpyHostToDevice);
 
-	dim3 gridSize(1,0,0);
-	dim3 blockSize(1,0,0);
+	dim3 gridSize2(1,0,0);
+	dim3 blockSize2(1,0,0);
 
-	bird_thread<<<gridSize,blockSize>>>(resultantMatrix,resultantAngle);
+	bird_thread<<<gridSize2,blockSize2>>>(resultantMatrix,resultantAngle,coords_row,coords_col);
 	
-	cudaMemcpy(pos_row,pos_rowPtr,TIMESTEPS * sizeof(int),cudaMemcpyDeviceToHost);
-	cudaMemcpy(pos_row,pos_colPtr,TIMESTEPS * sizeof(int),cudaMemcpyDeviceToHost);
-	
-	
-	//struct Data dataVals;
-	//dataVals.temp_row = pos_row;
-	//dataVals.temp_col = pos_col;
+	cudaMemcpy(coords_row,coords_rowPtr,TIMESTEPS * sizeof(int),cudaMemcpyDeviceToHost);
+	cudaMemcpy(coords_row,coords_colPtr,TIMESTEPS * sizeof(int),cudaMemcpyDeviceToHost);
 
-
-	cudaFree(pos_rowPtr);
-	cudaFree(pos_colPtr);
+	cudaFree(coords_rowPtr);
+	cudaFree(coords_colPtr);
 	cudaFree(resultantMatrixPtr);
 	cudaFree(resultantAnglePtr);
 
-	//return dataVals; 
+	//printf("%d,%d\n",coords_row[0],coords_col[0]);
+	*/
 	return 0;
 
 }
